0a4e078202c19fa85922eed33ee5722ad17e2930
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-16 18:31:14 -0300

    feat(TrybeGamesDatabase.cs): Método "GetGamesPlayedBy"
    Funcionalidade de buscar jogos jogados por uma pessoa jogadora

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -20,8 +20,10 @@ public class TrybeGamesDatabase
     // 5. Crie a funcionalidade de buscar jogos jogados por uma pessoa jogadora
     public List<Game> GetGamesPlayedBy(Player player)
     {
-        // Implementar
-        throw new NotImplementedException();  
+        var gamesPlayed = from game in Games
+                          where game.Players.Contains(player.Id)
+                          select game;
+        return gamesPlayed.ToList();
     }
 
     // 6. Crie a funcionalidade de buscar jogos comprados por uma pessoa jogadora
