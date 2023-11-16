72966fa9fa1a42a472935ec0e956136b5d08c3a6
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-16 18:26:44 -0300

    feat(TrybeGamesDatabase.cs): Método "GetGamesDevelopedBy"
    Funcionalidade de buscar jogos desenvolvidos por um estúdio de jogos

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -11,8 +11,10 @@ public class TrybeGamesDatabase
     // 4. Crie a funcionalidade de buscar jogos desenvolvidos por um estúdio de jogos
     public List<Game> GetGamesDevelopedBy(GameStudio gameStudio)
     {
-        // implementar
-        throw new NotImplementedException();
+        var gamesDeveloped = from game in Games
+                             where game.DeveloperStudio == gameStudio.Id
+                             select game;
+        return gamesDeveloped.ToList();
     }
 
     // 5. Crie a funcionalidade de buscar jogos jogados por uma pessoa jogadora
