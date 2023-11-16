7a8a80b2074ec5941aff074f8b1c31681fef92da
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-16 19:16:12 -0300

    feat(TrybeGamesDatabase.cs): Método "GetGameTypes"
    Funcionalidade de buscar todos os diferentes tipos de jogos dentre os jogos cadastrados no banco de dados

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -54,8 +54,8 @@ public class TrybeGamesDatabase
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
-        // Implementar
-        throw new NotImplementedException();
+        return Games.GroupBy(game => game.GameType)
+            .Select(groupGameTypes => groupGameTypes.Key).ToList();
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
